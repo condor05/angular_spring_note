fe805b55f9d44ae356e56ca3e8defca71b231b62
Xiang Wu <xiangw@cisco.com>
2017-05-26 17:01:41 +0800

    init commit of the project

---
+++ src/main/java/demo/WebSecurityConfig.java
@@ -0,0 +1,88 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+////        http
+////                .authorizeRequests()
+////                .antMatchers("/", "/home").permitAll()
+////                .anyRequest().authenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                .permitAll()
+////                .and()
+////                .logout()
+////                .permitAll();
+//        http
+//                .httpBasic().and()
+//                .authorizeRequests()
+//                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+//        // @formatter:on
+//    }
+////@Configuration
+////@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+////protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////        // @formatter:off
+////        http
+////                .httpBasic().and()
+////                .authorizeRequests()
+////                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+////                .anyRequest().authenticated()
+////                .and()
+////                .csrf()
+////                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+////        // @formatter:on
+////    }
+////}
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("admin").password("password").roles("USER");
+//    }
+//}
+
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	public  class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.httpBasic().and()
+				.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/test.html", "/").permitAll()
+					.anyRequest().authenticated()
+					.and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+		}
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("password").roles("USER");
+    }
+	}
